fa19d2b3bcdae9fc5c2a90ba969ae230e3216997
Bach Minh Quang <72701749+quangminh1212@users.noreply.github.com>
2024-10-02 22:22:25 +0700

    Update Project folder

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 from PyQt6.QtWidgets import QApplication
 
 from auth.google_auth import GoogleAuth
